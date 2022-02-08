a8c5a169fe6dba5f2d40f2dfda21e781a033bb5d
Dminon <97939550+Dminon@users.noreply.github.com>
2022-02-08 11:40:58 +0700

    add css label tamplate

--- src/service/CopperSkService.ts
+++ src/service/CopperSkService.ts
@@ -8,8 +8,9 @@ import { INodeType } from "../model/INodeType";
 export const copperSkApi = createApi({
   reducerPath: "copperSkApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v0",
+    // baseUrl: "http://localhost:8080/api/v0",
     // baseUrl: "http://192.168.30.149:18090/api/v0",
+    baseUrl: "http://192.168.30.149:18090/api/v0",
   }),
   tagTypes: ["Links", "Tails", "Nodes", "NodeTypes"],
   endpoints: (build) => ({
